8ca60308bd962159574a0ca79358901697b13b19
mohammad moghimi <mmoghimi2003@gmail.com>
2023-07-11 10:43:20 +0430

    first piece of code

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,2 +1,15 @@
-package main;public class Main {
+package main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame() ;
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("mypaintI.O");
+
+        window.setLocation(null);
+        window.setVisible(true);
+    }
 }
